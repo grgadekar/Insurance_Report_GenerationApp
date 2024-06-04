47233adb6a5410a409da6e79b24e7cd973e7a79e
grgadekar <ganesh.gadekar474@gmail.com>
2024-06-04 16:45:02 +0530

    Required classes and interfaces created

--- src/main/java/com/ganesh/controller/ReportController.java
+++ src/main/java/com/ganesh/controller/ReportController.java
@@ -3,6 +3,7 @@ package com.ganesh.controller;
 import com.ganesh.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReportController {
@@ -10,5 +11,9 @@ public class ReportController {
     @Autowired
     private ReportService service;
 
+    @GetMapping("/")
+    public String indexPage(){
+        return "index";
+    }
 
 }
